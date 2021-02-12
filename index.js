7f9a88c1e1e221ff62da093983ccead1e8f47322
popdev86 <speedwobble100@gmail.com>
2021-02-12 15:35:39 -0500

    -completed, need to refactor.

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ createAutoComplete({
         onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     },
 });
+
 createAutoComplete({
     ...autocompleteConfig,
     root: document.querySelector('#right-autocomplete'),
@@ -62,42 +63,80 @@ const onMovieSelect = async (movie, summaryElement, side) => {
         runComparison();
     }
 };
+
 const runComparison = () => {
-    console.log('time for comparison');
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+        const leftSideValue = leftStat.dataset.value;
+        const rightSideValue = rightStat.dataset.value;
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
+    });
 };
+
 const movieTemplate = (movieDetail) => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/, ''));
+    const metascores = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+    let count = 0;
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word);
+        if (isNaN(value)) {
+            return prev;
+        } else {
+            return prev + value;
+        }
+    }, 0);
+
     return `
+
         <article class ='media'>
-        <figure class ='media-left'>
-        <p class='image'>
-        <img src='${movieDetail.Poster}' alt=''/>
-</p>
-</figure>
-<div class='media-content'>
-<div class='content'>
-<h1>${movieDetail.Title}</h1>
-<h4>${movieDetail.Genre}</h4>
-<p>${movieDetail.Plot}</p>
-</div>
-</div>
+            <figure class ='media-left'>
+                <p class='image'>
+                    <img src='${movieDetail.Poster}' alt=''/>
+                </p>
+        </figure>
+    <div class='media-content'>
+        <div class='content'>
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
 </article>
-<article class='notification is-primary'>
+
+<article data-value='${awards}' class='notification is-primary'>
 <p class='title'>${movieDetail.Awards}</p>
 <p class='subtitle'>Awards</p>
 </article>
-<article class='notification is-primary'>
+
+<article data-value='${dollars}' class='notification is-primary'>
 <p class='title'>${movieDetail.BoxOffice}</p>
 <p class='subtitle'>Box Office</p>
 </article>
-<article class='notification is-primary'>
+
+<article data-value='${metascores}' class='notification is-primary'>
 <p class='title'>${movieDetail.Metascore}</p>
 <p class='subtitle'>Meta Score</p>
 </article>
-<article class='notification is-primary'>
+
+<article data-value='${imdbRating}' class='notification is-primary'>
 <p class='title'>${movieDetail.imdbRating}</p>
 <p class='subtitle'>Imdb Rating</p>
 </article>
-<article class='notification is-primary'>
+
+<article data-value='${imdbVotes}' class='notification is-primary'>
 <p class='title'>${movieDetail.imdbVotes}</p>
 <p class='subtitle'>Imbd Votes</p>
 </article>
